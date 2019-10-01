e34a986a3e8ad1fdd40e8dc4acef3dc098195cf4
miuranobuaki <miuranobuak@gmail.com>
2019-10-01 19:33:31 +0900

    画像加工処理を追加

--- src/utils/URLUtils.ts
+++ src/utils/URLUtils.ts
@@ -1,19 +1,40 @@
+import { SponsorGrade } from '../common/enum/sheet-enum';
+
 /**
  * URLがらみの設定をするUTILクラス
  */
 export class URLUtils {
-  static speakerImageURL(rowNum: number, extention: string): string {
-    const num: string = ('000' + rowNum).slice(-3);
-    return '/assets/images/speakers/speaker' + num + extention;
+  static speakerImageURL(image: string): string {
+    return '/assets/images/speakers/' + image;
   }
 
-  static sponsorImageURL(rowNum: number, extention: string): string {
-    const num: string = ('000' + rowNum).slice(-3);
-    return '/assets/images/sponsors/sponsor' + num + extention;
+  static sponsorImageURL(grade: string, fileName: string): string {
+    let prefix: string = '/assets/images/sponsors/';
+    switch (grade) {
+      case SponsorGrade.platinam:
+        return prefix + 'platina/' + fileName;
+      case SponsorGrade.gold:
+        return prefix + 'gold/' + fileName;
+      case SponsorGrade.silver:
+        return prefix + 'silver/' + fileName;
+      case SponsorGrade.bronze:
+        return prefix + 'bronze/' + fileName;
+      case SponsorGrade.logo:
+        return prefix + 'logo/' + fileName;
+      case SponsorGrade.drink:
+        return prefix + 'drink/' + fileName;
+      default:
+        // TODO: PMConfのデフォルト画像を入れる
+        return '';
+    }
   }
 
   static personalDefaultImageURL(): string {
     return '/assets/images/pmconf_ico.png';
+  }
+
+  static personalImageURL(image: string): string {
+    return '/assets/images/sponsors/personal/' + image;
   }
 
   static staffImageURL(image: string): string {
